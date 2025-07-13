9ccf2e6bd9328a32cc91dee1c0e8d28e9849c4c5
defenderwidi <defenderwidi17@gmail.com>
2025-07-13 19:40:58 +0700

    siap kerja 4

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import Hero from './Hero';
 import TechStack from './TechStack';
-import MyWork from './MyWork';
+import MyWork from './mywork';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
